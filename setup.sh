fd4e10d47b085ba08b3c0981e6446ba3e805dbd9
gldanoob <caleblolhk@gmail.com>
2023-09-21 02:52:30 +0100

    updated entry point

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade PySide6 openai tiktoken gtts pyqtdarktheme duckduckgo-search googlesearch-python
-python3 ChatGPT-GUI.py
+python3 main.py
