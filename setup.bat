7b149055f8c36f7ca2402c32302f958c3cf542bc
Eliran Wong <hkwebtech@gmail.com>
2023-12-23 22:33:40 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 python -m venv venv
 call .\venv\Scripts\activate
-pip install --upgrade PySide6 openai tiktoken gtts pyqtdarktheme duckduckgo-search googlesearch-python
+pip install --upgrade PySide6 openai==0.28.1 tiktoken gtts pyqtdarktheme duckduckgo-search googlesearch-python
 python ChatGPT-GUI.py
 pause
