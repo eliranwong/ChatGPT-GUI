d281e96038db388aa75e1f3269c7e0d9143ee64f
Eliran Wong <hkwebtech@gmail.com>
2023-12-23 22:33:59 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv
 source venv/bin/activate
-pip3 install --upgrade PySide6 openai tiktoken gtts pyqtdarktheme duckduckgo-search googlesearch-python
+pip3 install --upgrade PySide6 openai==0.28.1 tiktoken gtts pyqtdarktheme duckduckgo-search googlesearch-python
 python3 ChatGPT-GUI.py
